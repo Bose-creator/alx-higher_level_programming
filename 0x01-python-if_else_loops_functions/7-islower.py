acde38fad0e0bb2442c1e200be36d5c526e8ab48
Bose Aletogbe <irawomartins@gmail.com>
2024-01-03 05:25:40 +0100

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-
 def islower(c):
     """
     Check if a character is lowercase.
-
     Args:
     c (str): A single character
-
     Returns:
     bool: True if c is lowercase, False otherwise
     """
     return ord('a') <= ord(c) <= ord('z')
-
-# Example usage:
-print(islower('a'))  # Should return True
-print(islower('z'))  # Should return True
-print(islower('A'))  # Should return False
-print(islower('Z'))  # Should return False
-print(islower('1'))  # Should return False
-print(islower('?'))  # Should return False
+print(islower('a'))
+print(islower('z'))
+print(islower('A'))
+print(islower('Z'))
+print(islower('1'))
+print(islower('?'))
